60c5a6c5874864b63081da7a7b8592e0cdad968d
MinatoMinazuki <141279068+MinatoMinazuki@users.noreply.github.com>
2024-09-17 07:43:07 +0900

    情報登録

--- files/regiInfo.php
+++ files/regiInfo.php
@@ -11,8 +11,8 @@ $stage = "0";
 if( !empty($_POST) ){
 
     $stage = h($_POST['stage']);
+
     $name = h($_POST['name']);
-    $area = h($_POST['area']);
     $prefecture = h($_POST['prefecture']);
     $place = h($_POST['place']);
     $date = h($_POST['date']);
@@ -31,7 +31,6 @@ if( !empty($_POST) ){
 if( $stage === "1" ){
 
     if( empty($name)
-        || empty($name)
         || empty($place)
         || empty($date)
         || empty($tel)
@@ -42,7 +41,6 @@ if( $stage === "1" ){
     } else {
 
         $name_hide = empty($name) ? "hide" : "";
-        $area_hide = empty($area) ? "hide" : "";
         $prefecture_hide = empty($prefecture) ? "hide" : "";
         $place_hide = empty($place) ? "hide" : "";
         $date_hide = empty($date) ? "hide" : "";
@@ -75,6 +73,8 @@ if( $stage === "1" ){
 
     $fileName = "";
     $fileName_2 = "";
+
+    $area = areaPrefecture( $prefecture );
 
     if( !empty($_POST['image']) ){
         $data = base64Decode( $_POST['image'] );
@@ -198,6 +198,86 @@ function randomString(){
     return $tmp;
 }
 
+function prefectureArr(){
+    return $prefectures = [1 => "北海道",
+                           2 => "青森県",
+                           3 => "岩手県",
+                           4 => "宮城県",
+                           5 => "秋田県",
+                           6 => "山形県",
+                           7 => "福島県",
+                           8 => "茨城県",
+                           9 => "栃木県",
+                           10 => "群馬県",
+                           11 => "埼玉県",
+                           12 => "千葉県",
+                           13 => "東京都",
+                           14 => "神奈川県",
+                           15 => "新潟県",
+                           16 => "富山県",
+                           17 => "石川県",
+                           18 => "福井県",
+                           19 => "山梨県",
+                           20 => "長野県",
+                           21 => "岐阜県",
+                           22 => "静岡県",
+                           23 => "愛知県",
+                           24 => "三重県",
+                           25 => "滋賀県",
+                           26 => "京都府",
+                           27 => "大阪府",
+                           28 => "兵庫県",
+                           29 => "奈良県",
+                           30 => "和歌山県",
+                           31 => "鳥取県",
+                           32 => "島根県",
+                           33 => "岡山県",
+                           34 => "広島県",
+                           35 => "山口県",
+                           36 => "徳島県",
+                           37 => "香川県",
+                           38 => "愛媛県",
+                           39 => "高知県",
+                           40 => "福岡県",
+                           41 => "佐賀県",
+                           42 => "長崎県",
+                           43 => "熊本県",
+                           44 => "大分県",
+                           45 => "宮崎県",
+                           46 => "鹿児島県",
+                           47 => "沖縄県"];
+}
+
+function areaPrefecture($value){
+
+    $prefectures = prefectureArr();
+
+    foreach($prefectures as $k => $pref){
+        if( $pref === $value ){
+            if( $k === 1 ){
+                return "北海道";
+            } elseif( 2 <= $k && 7>= $k ){
+                return "東北";
+            } elseif( 8 <= $k && 14 >= $k ){
+                return "関東";
+            } elseif( 15 <= $k && 18 >= $k ){
+                return "北陸";
+            } elseif( 19 <= $k && 23 >= $k ){
+                return "東海";
+            } elseif( 24 <= $k && 30 >= $k ){
+                return "関西";
+            } elseif( 31 <= $k && 35 >= $k ){
+                return "中国";
+            } elseif( 36 <= $k && 39 >= $k ){
+                return "四国";
+            } elseif( 30 <= $k && 47 >= $k ){
+                return "九州";
+            }
+        }
+    }
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -213,141 +293,185 @@ function randomString(){
     </style>
 </head>
 <body>
+    <h1>公演情報の登録</h1>
     <div class="wrapper">
-        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" id="formTheaterInfo">
             <?php if( !empty($message["error"]) ) : ?>
-            <div>
-                <p>
-                    <?= $message["error"] ?>
+            <div class="message">
+                <p class="error">
+                    <?= $message["error"]; ?>
                 </p>
                 <a href="<?= $_SERVER['PHP_SELF']; ?>">戻る</a>
             <?php elseif( !empty($message["comp"]) ) : ?>
-                <p>
-                    <?= $message["comp"] ?>
+                <p class="comp">
+                    <?= $message["comp"]; ?>
                 </p>
                 <a href="index.php">戻る</a>
             </div>
             <?php elseif( $stage === "0" ) : ?>
-            <div>
-                <p>
+            <div class="inputInfo">
+                <p class="theaterInfo theaterName">
                     <input type="text" name="name" value="" placeholder="公演名">
                 </p>
-                <p>
-                    <input type="text" name="area" value="" placeholder="地区">
-                </p>
-                <p>
+                <p class="theaterInfo theaterPrefecture">
                     <input type="text" name="prefecture" value="" placeholder="開催県">
                 </p>
-                <p>
-                    <input type="text" name="place" value="" placeholder="開催地">
+                <p class="theaterInfo theaterPlace">
+                    <input type="text" name="place" value="" placeholder="場所・施設名">
                 </p>
-                <p>
-                    <input type="text" name="date" value="" placeholder="開催日">
+                <p class="theaterInfo theaterDate">
+                    <input type="date" name="date" value="" placeholder="開催日">
                 </p>
-                <p>
-                    <input type="text" name="date_sub" value="" placeholder="開催日時の注意書き">
+                <p class="theaterInfo theaterDateSub">
+                    <textarea name="date_sub" value="" placeholder="開催日時の注意書き"></textarea>
                 </p>
-                <p>
+                <p class="theaterInfo theaterStartTime">
                     <input type="text" name="start_time" value="" placeholder="開演時間">
                 </p>
-                <p>
-                    <input type="text" name="tel" value="" placeholder="電話番号">
+                <p class="theaterInfo theaterTel">
+                    <input type="tel" name="tel" value="" placeholder="電話番号">
                 </p>
-                <p>
+                <p class="theaterInfo thaterTelAddress">
                     <input type="text" name="tel_address" value="" placeholder="登録電話番号の宛先">
                 </p>
-                <p>
+                <p class="theaterInfo theaterAddress">
                     <input type="text" name="address" value="" placeholder="開催地住所">
                 </p>
-                <p>
+                <p class="theaterInfo theaterAccess">
                     <input type="text" name="access" value="" placeholder="アクセス">
                 </p>
-                <p>
-                    <input type="text" name="parking" value="" placeholder="駐車場について">
+                <p class="theaterInfo theaterParking">
+                    <textarea name="parking" value="" placeholder="駐車場について"></textarea>
                 </p>
-                <p>
-                    <input type="text" name="url" value="" placeholder="URL">
+                <p class="theaterInfo theaterUrl">
+                    <input type="url" name="url" value="" placeholder="URL">
                 </p>
-                <p>
+                <p class="theaterInfo theaterImage">
                     <input type="file" name="image" value="">
                 </p>
-                <p>
+                <p class="theaterInfo theaterImage_2">
                     <input type="file" name="image_2" value="">
                 </p>
-                <button type="submit">確認</button>
+                <button class="submitBtn checkBtn" type="submit">確認</button>
                 <input type="hidden" name="stage" value="1">
             </div>
             <?php elseif( $stage === "1" ): ?>
-            <div>
-                <p>
+            <div class="cheachInfo">
+                <p class="theaterInfo theaterName">
                     <span class="name <?= $name_hide ?>"><?= $name ?></span>
                     <input type="hidden" name="name" value="<?= $name ?>" placeholder="公演名">
                 </p>
-                <p>
-                    <span class="area <?= $area_hide ?>"><?= $area ?></span>
-                    <input type="hidden" name="area" value="<?= $area ?>" placeholder="地区">
-                </p>
-                <p>
+                <p class="theaterInfo theaterPrefecture">
                     <span class="prefecture <?= $prefecture_hide ?>"><?= $prefecture ?></span>
                     <input type="hidden" name="prefecture" value="<?= $prefecture ?>" placeholder="開催県">
                 </p>
-                <p>
+                <p class="theaterInfo theaterPlace">
                     <span class="place <?= $place_hide ?>"><?= $place ?></span>
                     <input type="hidden" name="place" value="<?= $place ?>" placeholder="開催地">
                 </p>
-                <p>
+                <p class="theaterInfo theaterDate">
                     <span class="date <?= $date_hide ?>"><?= $date ?></span>
                     <input type="hidden" name="date" value="<?= $date ?>" placeholder="開催日">
                 </p>
-                <p>
+                <p class="theaterInfo theaterDateSub">
                     <span class="date_sub <?= $date_sub_hide ?>"><?= $date_sub ?></span>
                     <input type="hidden" name="date_sub" value="<?= $date_sub ?>" placeholder="開催日時の注意書き">
                 </p>
-                <p>
+                <p class="theaterInfo theaterStartTime">
                     <span class="start_time <?= $start_time_hide ?>"><?= $start_time ?></span>
                     <input type="hidden" name="start_time" value="<?= $start_time ?>" placeholder="開演時間">
                 </p>
-                <p>
+                <p class="theaterInfo theaterTel">
                     <span class="tel <?= $tel_hide ?>"><?= $tel ?></span>
                     <input type="hidden" name="tel" value="<?= $tel ?>" placeholder="電話番号">
                 </p>
-                <p>
+                <p class="theaterInfo thaterTelAddress">
                     <span class="tel_address <?= $tel_address_hide ?>"><?= $tel_address ?></span>
                     <input type="hidden" name="tel_address" value="<?= $tel_address ?>" placeholder="登録電話番号の宛先">
                 </p>
-                <p>
+                <p class="theaterInfo theaterAddress">
                     <span class="address <?= $address_hide ?>"><?= $address ?></span>
                     <input type="hidden" name="address" value="<?= $address ?>" placeholder="開催地住所">
                 </p>
-                <p>
+                <p class="theaterInfo theaterAccess">
                     <span class="access <?= $access_hide ?>"><?= $access ?></span>
                     <input type="hidden" name="access" value="<?= $access ?>" placeholder="アクセス">
                 </p>
-                <p>
+                <p class="theaterInfo theaterParking">
                     <span class="parking <?= $parking_hide ?>"><?= $parking ?></span>
                     <input type="hidden" name="parking" value="<?= $parking ?>" placeholder="駐車場について">
                 </p>
-                <p>
+                <p class="theaterInfo theaterUrl">
                     <span class="url <?= $url_hide ?>"><?= $url ?></span>
                     <input type="hidden" name="url" value="" placeholder="URL">
                 </p>
                 <?php if(!empty($img)) : ?>
-                <div>
+                <div class="theaterInfo theaterImage">
                     <img src="data:<?= $fileType; ?>;base64,<?= $img; ?>" alt="１枚目の画像です。" style="width: 300px;">
                     <input type="hidden" name="image" value="data:<?= $fileType; ?>;base64,<?= $img; ?>">
                 </div>
                 <?php endif; ?>
                 <?php if(!empty($img_2)) : ?>
-                <div>
+                <div class="theaterInfo theaterImage_2">
                     <img src="data:<?= $fileType_2; ?>;base64,<?= $img_2; ?>" alt="２枚目の画像です。" style="width: 300px;">
                     <input type="hidden" name="image_2" value="data:<?= $fileType_2; ?>;base64,<?= $img_2; ?>">
                 </div>
                 <?php endif; ?>
-                <button type="submit">登録する</button>
                 <input type="hidden" name="stage" value="2">
+                <button class="submitBtn regiBtn" type="submit">登録する</button>
             </div>
             <?php endif; ?>
         </form>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(function(){
+
+            $("#formTheaterInfo").submit(function(e){
+
+                var emptyValue = [];
+
+                var name = $(".theaterName").find("input").val(),
+                    place = $(".theaterPlace").find("input").val(),
+                    eventDate = $(".theaterDate").find("input").val(),
+                    tel = $(".theaterTel").find("input").val(),
+                    address = $(".theaterAddress").find("input").val();
+
+                if( name === "" ){
+                    emptyValue.push("公演名");
+                }
+
+                if( place === "" ){
+                    emptyValue.push("開催場所");
+                }
+
+                if( eventDate === "" ){
+                    emptyValue.push("開催日時");
+                }
+
+                if( tel === "" ){
+                    emptyValue.push("電話番号");
+                }
+
+                if( address === "" ){
+                    emptyValue.push("開催地の住所");
+                }
+
+                if( emptyValue.length > 0 ){
+
+                    var emptyString = emptyValue.join(",");
+                    alert("入力内容に不備があります。次の項目を入力してください。"+ emptyString);
+                    return false;
+
+                } else {
+
+                    $(this).submit();
+
+                }
+
+            });
+
+        });
+    </script>
 </body>
 </html>
