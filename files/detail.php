0bc506061b9d67ef270895ab942ee3e5fc68c420
MinatoMinazuki <141279068+MinatoMinazuki@users.noreply.github.com>
2024-10-19 09:13:00 +0900

    Update detail.php

--- files/detail.php
+++ files/detail.php
@@ -19,3 +19,30 @@ $result = $dbc->select($sql);
 $item = $result[0];
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?= $item["name"] ?>の詳細ページ</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <h1><?= $item["name"] ?></h1>
+    <div>
+        <table>
+            <tr>
+                <th>公演名</th>
+                <th>開催県</th>
+                <th>開催地</th>
+                <th>開催日</th>
+                <th>公演名</th>
+                <th>公演名</th>
+                <th>公演名</th>
+                <th>公演名</th>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
